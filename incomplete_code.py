b5eb9bb1c2d3bc8d4257a9cdde4cf15102382b35
Pun <80048553+thammanant@users.noreply.github.com>
2023-02-09 18:13:44 +0700

    update fins cost

--- incomplete_code.py
+++ incomplete_code.py
@@ -1,80 +1,82 @@
 class Transportation(object):
-   """Abstract base class"""
+    """Abstract base class"""
 
-   def __init__( self, start, end, distance ):
-      if self.__class__ == Transportation:
-         raise NotImplementedError
-      self.start = start
-      self.end = end
-      self.distance = distance
+    def __init__(self, start, end, distance):
+        if self.__class__ == Transportation:
+            raise NotImplementedError
+        self.start = start
+        self.end = end
+        self.distance = distance
 
-   def find_cost( self ):
-      """Abstract method; derived classes must override"""
-      raise NotImplementedError
+    def find_cost(self):
+        """Abstract method; derived classes must override"""
+        raise NotImplementedError
 
 
-class Walk( Transportation ):
+class Walk(Transportation):
 
-   def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
+    def __init__(self, start, end, distance):
+        Transportation.__init__(self, start, end, distance)
 
-   def start_place( self ):
+    def start_place(self):
         return self.start
 
-   def end_place( self ):
+    def end_place(self):
         return self.end
 
-   def distance( self ):
+    def distance(self):
         return self.distance
 
+    def find_cost(self):
+        return 0
 
-   def find_cost( self ):
-      return 0
 
 class Taxi(Transportation):
 
-   def __init__( self, start, end, distance ):
-      Transportation.__init__( self, start, end, distance)
+    def __init__(self, start, end, distance):
+        Transportation.__init__(self, start, end, distance)
 
-   def find_cost( self ):
-      return 40*self.distance
+    def find_cost(self):
+        return 40 * self.distance
 
-   def start_place(self):
+    def start_place(self):
         return self.start
 
-   def end_place(self):
+    def end_place(self):
         return self.end
 
-   def distance(self):
+    def distance(self):
         return self.distance
-class Train( Transportation ):
 
-   def __init__( self, start, end, distance, fare ):
-      Transportation.__init__( self, start, end, distance)
-      self.fare = fare
 
-   def find_cost( self ):
-      return self.fare * self.distance
+class Train(Transportation):
 
-   def start_place( self ):
+    def __init__(self, start, end, distance, fare):
+        Transportation.__init__(self, start, end, distance)
+        self.fare = fare
+
+    def find_cost(self):
+        return self.fare * 5
+
+    def start_place(self):
         return self.start
 
-   def end_place( self ):
+    def end_place(self):
         return self.end
 
-   def distance( self ):
+    def distance(self):
         return self.distance
 
-   
+
 # main program
 
 travel_cost = 0
 
-trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
-         Taxi("KMITL SCB Bank","Ladkrabang Station",5),
-         Train("Ladkrabang Station","Payathai Station",40,6),
-         Taxi("Payathai Station","The British Council",3) ]
+trip = [Walk("KMITL", "KMITL SCB Bank", 0.6),
+        Taxi("KMITL SCB Bank", "Ladkrabang Station", 5),
+        Train("Ladkrabang Station", "Payathai Station", 40, 6),
+        Taxi("Payathai Station", "The British Council", 3)]
 
 for travel in trip:
-   travel_cost += travel.find_cost()
+    travel_cost += travel.find_cost()
 print(travel_cost)
